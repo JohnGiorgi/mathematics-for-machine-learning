f37bf81184b743bbffd525e0537bdd1b811f0ffa
johngiorgi <johnmgiorgi@gmail.com>
2018-10-10 20:05:12 -0400

    :art: Reduce intensity of parallax effect

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -2,7 +2,5 @@
 var images = document.getElementsByTagName('img');
 for (var i = 0; images[i]; i++) {
      images[i].setAttribute('data-tilt', '');
-     images[i].setAttribute('data-tilt-axis', 'x');
-     images[i].setAttribute('data-tilt-glare', 'true');
-     images[i].setAttribute('data-tilt-maxGlare', '0.5');
+     images[i].setAttribute('data-tilt-perspective', 1800);
 }
